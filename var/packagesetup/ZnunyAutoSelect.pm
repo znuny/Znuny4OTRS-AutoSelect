ede7133b946114ee393876b4fa37374fcc4c6de4
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2025-09-29 09:04:45 +0200

    Applied CodePolicy.

--- var/packagesetup/ZnunyAutoSelect.pm
+++ var/packagesetup/ZnunyAutoSelect.pm
@@ -182,7 +182,7 @@ sub _MigrateSysConfigSettings {
 
             $LogObject->Log(
                 Priority => 'error',
-                Message =>
+                Message  =>
                     "Error: Unable to migrate value of SysConfig option $OriginalSysConfigOptionName to option $NewSysConfigOptionName",
             );
         }
